3e66b5d9f2d8f275653a1328b933b6689b930fff
euLita <litas.educ@gmail.com>
2022-07-22 18:24:44 -0400

    finished class 2 still on methods

---
+++ MetodosAula02/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Resultado do exercitando Aula02. ");
+        System.out.println(" Exercício Quadrilátero. ");
+
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
